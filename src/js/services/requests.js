6261774f58a1dd35cf1079730051eceda8a2e93e
nairelyhz <njhs694@gmail.com>
2019-09-26 07:24:37 -0400

    pruebas sobre health

--- src/js/services/requests.js
+++ src/js/services/requests.js
@@ -3,16 +3,16 @@ const axios = require('axios');
 
 module.exports = {
   get_health : function (){
-    axios.get('http://localhost:8080/health',{
-      "Access-Control-Allow-Origin":"*",
-      'Access-Control-Allow-Methods' : 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"})
-    .then(response => {
-      alert(response);
-  })
-  .catch(error => {
-      reject(error);
-  });
+    return new Promise((resolve,reject)=>{
+      axios.get('http://0.0.0.0/health')
+      .then(response => {
+        resolve(JSON.stringify(response));
+    })
+    .catch(error => {
+        reject(error);
+    });
+    });
+    
     
   },
   
