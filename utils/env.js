e459102657d4cffac906c0f6192a1c3269e02c72
fabiolamartinez <fabiola.martinez@talan.com>
2019-10-08 17:37:32 -0400

    update env

--- utils/env.js
+++
@@ -1,8 +0,0 @@
-import config from 'dotenv-webpack'
-config()
-
-// tiny wrapper with default env vars
-export default {
-  NODE_ENV: (process.env.NODE_ENV || 'development'),
-  PORT: (3000)
-}
---
+++ utils/env.ts
@@ -0,0 +1,5 @@
+// tiny wrapper with default env vars
+export default {
+  NODE_ENV: ('development'),
+  PORT: (3000)
+}
