0e7f3801fa64ae68f4ceda8b221fb9ce6ddb69d5
German Robayo <germanrobayo33@gmail.com>
2020-06-26 09:54:56 -0400

    fix(auditing): bump some packages to erase vulnerabilities (#45)

--- webpack.config.js
+++ webpack.config.js
@@ -84,17 +84,19 @@ const options = {
     // expose and write the allowed env vars on the compiled bundle
     new webpack.EnvironmentPlugin(['NODE_ENV']),
     new Dotenv(),
-    new CopyWebpackPlugin([{
-      from: 'src/manifest.json',
-      transform: function (content, path) {
-        // generates the manifest file using the package.json informations
-        return Buffer.from(JSON.stringify({
-          ...JSON.parse(content.toString()),
-          description: process.env.npm_package_description,
-          version: process.env.npm_package_version
-        }))
-      }
-    }]),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: 'src/manifest.json',
+        transform: function (content, path) {
+          // generates the manifest file using the package.json informations
+          return Buffer.from(JSON.stringify({
+            ...JSON.parse(content.toString()),
+            description: process.env.npm_package_description,
+            version: process.env.npm_package_version
+          }))
+        }
+      }]
+    }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'popup.html'),
       filename: 'popup.html',
